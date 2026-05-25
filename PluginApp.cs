cdf2bc3c406d0d467b671121a70693d19edd86cd
marcogiffi-a11y <marcogiffi@gmail.com>
2026-05-25 13:07:49 +0200

    fix: register PluginApp as CommandClass for CHECK_UPDATE

--- PluginApp.cs
+++ PluginApp.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Windows.Forms.Integration;
 
 [assembly: CommandClass(typeof(ImplantiAI.Commands))]
+[assembly: CommandClass(typeof(ImplantiAI.PluginApp))]
 [assembly: ExtensionApplication(typeof(ImplantiAI.PluginApp))]
 
 namespace ImplantiAI
