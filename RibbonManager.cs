2058c56c837a119118a23d1a204c1e6b56f045f3
marcogiffi-a11y <marcogiffi-a11y@users.noreply.github.com>
2026-05-27 11:08:18 +0000

    fix(build): aggiungi using System.Windows.Media.Imaging in RibbonManager (BitmapSource/BitmapImage)

--- RibbonManager.cs
+++ RibbonManager.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace ImplantiAI
 {
