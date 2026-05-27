a126dcb14b0f5ca4ad4976117dc16afd1e6c80e4
marcogiffi-a11y <marcogiffi-a11y@users.noreply.github.com>
2026-05-27 10:26:20 +0000

    fix(build): rimuovo 'using System.Windows' (Application ambiguo con AutoCAD)
    L'import 'using System.Windows' introduceva ambiguita tra
    System.Windows.Application e Autodesk.AutoCAD.ApplicationServices.Application
    usato in SymbolLibrary linea 141. Tutti i tipi WPF in RenderPreview
    sono gia' fully-qualified (System.Windows.Point, System.Windows.Media.Pen
    etc), quindi l'using non serviva.

--- SymbolLibrary.cs
+++ SymbolLibrary.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
